cc6cb6c102ba44c69a131c2da2e0ffca7b18fa8a
Ciaran Morinan <cmorinan@gmail.com>
2018-10-07 16:43:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,9 @@ function calculatesFarePrice(start, end) {
   } else if (distance > 400 && distance < 2000) {
     return (distance - 400) * 0.02;
   } else if (distance > 2000 && distance < 2500) {
-    return 
+    return 25;
+  } else {
+    return 'cannot travel that far';
   }
 }
 
